0a97b66c12adc84ea9d485862fcc782eebd1f4b0
Lakshay Khandelwal <lk20051402@gmail.com>
2026-05-23 08:30:26 +0530

    Day 5 Practice

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print("hello world")
+import sqlite3
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+sql="""
+insert into emp values(1,"Lakshay","Jaipur","98985")
+    
+"""
+conn.execute(sql)
+conn.commit()
+conn.close()
